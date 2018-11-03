1a2ebab0ffd73f17be84c740190b2df281c2c56a
Daniel Schulz <faulolio@gmail.com>
2018-11-03 10:39:42 +0100

    NPC Modelling

---
+++ Communiganda/Assets/Scripts/SpecimenBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class SpecimenBehavior : MonoBehaviour {
+    [SerializeField] private Specimen id;
+    [SerializeField] private Specimen[] engagesWith;
+    [SerializeField] private Channel[] sendsVia;
+    [SerializeField] private Channel[] receivesVia;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
