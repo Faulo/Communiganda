2149a84207b7b3b2609f2348ddfe85f585f1c299
sven schäfer <sven-schaefer@gmx.net>
2018-11-03 01:02:35 +0100

    Added basic player controls and z Order.

---
+++ Communiganda/Assets/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 10;
+    [SerializeField] private float speechAttackRadius = 3f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        HandlePlayerInput();
+    }
+
+    private void HandlePlayerInput()
+    {
+        var input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        transform.Translate(input * moveSpeed * Time.deltaTime);
+    }
+
+    private void SpeechAttack()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, speechAttackRadius);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+           // colliders[i].GetComponent<XY>
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, speechAttackRadius);
+    }
+}
